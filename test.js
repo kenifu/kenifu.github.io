365b6c0fc595f13f8dc56acbfc3be70c0076adaa
kenifu <72307179+kenifu@users.noreply.github.com>
2020-11-15 14:38:50 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+ <!--
+function PassCheck(){
+ var num = prompt("パスワードを入力して下さい","");
+if(num == "java"){
+ location.href="http://www.rakuten.ne.jp/gold/shop-name/abc/";
+ } else {
+ alert("パスワードが間違っています。");
+ }
+ }
+ //-->
+ </script>
+ <form>
+ここから先は会員だけです。→ 
+ <input type="button" value="会員制サイトに入る" onClick="PassCheck()">
+ </form>
